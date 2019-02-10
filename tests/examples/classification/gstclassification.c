81fdb6ec69607dc85f4df14924dc9614a5700368
GFallasRR <greivin.fallas@ridgerun.com>
2019-02-10 02:54:22 -0600

    Remove unused code

--- tests/examples/classification/gstclassification.c
+++ tests/examples/classification/gstclassification.c
@@ -1,27 +1,13 @@
 /* GstClassification
- * Copyright (C) 2019 FIXME <fixme@example.com>
- * Copyright (C) 2010 Entropy Wave Inc
+ * Copyright (C) 2019 RidgeRun <support@ridgerun.com>
+ * All Rights Reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ *The contents of this software are proprietary and confidential to RidgeRun,
+ *LLC.  No part of this program may be photocopied, reproduced or translated
+ *into another programming language without prior written consent of
+ *RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ *a software license from RidgeRun.  All source code changes must be provided
+ *back to RidgeRun without any encumbrance.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -29,46 +15,43 @@
 #endif
 
 #include <gst/gst.h>
+#include <glib-unix.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "gst/r2inference/gstinferencemeta.h"
 
-#define GETTEXT_PACKAGE "classification"
-
+#define GETTEXT_PACKAGE "GstInference"
 
 typedef struct _GstClassification GstClassification;
 struct _GstClassification
 {
   GstElement *pipeline;
-  GstBus *bus;
   GMainLoop *main_loop;
-
   GstElement *inference_element;
-
-  gboolean paused_for_buffering;
-  guint timer_id;
 };
 
 GstClassification *gst_classification_new (void);
 void gst_classification_free (GstClassification * classification);
 void gst_classification_create_pipeline (GstClassification * classification);
-void gst_classification_create_pipeline_playbin (GstClassification *
-    classification, const char *uri);
 void gst_classification_start (GstClassification * classification);
 void gst_classification_stop (GstClassification * classification);
-
+static void gst_classification_process_inference (GstElement * element,
+    GstClassificationMeta * meta, GstBuffer * buffer, gpointer user_data);
+static gboolean gst_classification_exit_handler (gpointer user_data);
 static gboolean gst_classification_handle_message (GstBus * bus,
     GstMessage * message, gpointer data);
-static gboolean onesecond_timer (gpointer priv);
-
-
 gboolean verbose;
+static const gchar *model_path = NULL;
+static const gchar *file_path = NULL;
+static const gchar *backend = NULL;
 
 static GOptionEntry entries[] = {
   {"verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL},
-
+  {"model", 'm', 0, G_OPTION_ARG_STRING, &model_path, "Model path", NULL},
+  {"file", 'f', 0, G_OPTION_ARG_STRING, &file_path, "File path", NULL},
+  {"backend", 'b', 0, G_OPTION_ARG_STRING, &backend,
+      "Backend used for inference, example: tensorflow", NULL},
   {NULL}
-
 };
 
 int
@@ -79,31 +62,47 @@ main (int argc, char *argv[])
   GstClassification *classification;
   GMainLoop *main_loop;
 
-  context = g_option_context_new ("- FIXME");
+  context = g_option_context_new ("GstInference Classification Example");
   g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
   g_option_context_add_group (context, gst_init_get_option_group ());
   if (!g_option_context_parse (context, &argc, &argv, &error)) {
-    g_print ("option parsing failed: %s\n", error->message);
+    g_printerr ("option parsing failed: %s\n", error->message);
     g_clear_error (&error);
     g_option_context_free (context);
     exit (1);
   }
   g_option_context_free (context);
 
-  classification = gst_classification_new ();
-
-  if (argc > 1) {
-    gchar *uri;
-    if (gst_uri_is_valid (argv[1])) {
-      uri = g_strdup (argv[1]);
-    } else {
-      uri = g_filename_to_uri (argv[1], NULL, NULL);
-    }
-    gst_classification_create_pipeline_playbin (classification, uri);
-    g_free (uri);
-  } else {
-    gst_classification_create_pipeline (classification);
+  if (verbose) {
+    g_print ("Model Path: %s \n", model_path);
+    g_print ("File path: %s \n", file_path);
+    g_print ("Backend: %s \n", backend);
   }
+
+  if (!backend) {
+    g_printerr ("Backend is required for inference (-b <backend>) \n");
+    exit (1);
+  }
+
+  if (!model_path) {
+    g_printerr ("Model path is required (-m <path>) \n");
+    exit (1);
+  }
+
+  if (!file_path) {
+    g_printerr ("File path is required (-f <path>) \n");
+    exit (1);
+  }
+
+  classification = gst_classification_new ();
+  gst_classification_create_pipeline (classification);
+
+  g_unix_signal_add (SIGINT, gst_classification_exit_handler, classification);
+  classification->inference_element =
+      gst_bin_get_by_name (GST_BIN (classification->pipeline), "net");
+
+  g_signal_connect (classification->inference_element, "new-prediction",
+      G_CALLBACK (gst_classification_process_inference), classification);
 
   gst_classification_start (classification);
 
@@ -112,9 +111,24 @@ main (int argc, char *argv[])
 
   g_main_loop_run (main_loop);
 
+  gst_classification_stop (classification);
+  gst_classification_free (classification);
+
   exit (0);
 }
 
+static gboolean
+gst_classification_exit_handler (gpointer data)
+{
+  GstClassification *classification;
+
+  g_return_val_if_fail (data, FALSE);
+
+  classification = (GstClassification *) data;
+  g_main_loop_quit (classification->main_loop);
+
+  return TRUE;
+}
 
 GstClassification *
 gst_classification_new (void)
@@ -129,6 +143,9 @@ gst_classification_new (void)
 void
 gst_classification_free (GstClassification * classification)
 {
+
+  g_return_if_fail (classification);
+
   if (classification->inference_element) {
     gst_object_unref (classification->inference_element);
     classification->inference_element = NULL;
@@ -139,59 +156,38 @@ gst_classification_free (GstClassification * classification)
     gst_object_unref (classification->pipeline);
     classification->pipeline = NULL;
   }
+  if (classification->main_loop) {
+    g_main_loop_unref (classification->main_loop);
+    classification->main_loop = NULL;
+  }
+
   g_free (classification);
 }
 
-void
-gst_classification_create_pipeline_playbin (GstClassification * classification,
-    const char *uri)
-{
-  GstElement *pipeline;
-  GError *error = NULL;
-
-  pipeline = gst_pipeline_new (NULL);
-  gst_bin_add (GST_BIN (pipeline),
-      gst_element_factory_make ("playbin", "source"));
-
-  if (error) {
-    g_print ("pipeline parsing error: %s\n", error->message);
-    g_clear_error (&error);
-    gst_object_unref (pipeline);
-    return;
-  }
-
-  classification->pipeline = pipeline;
-
-  gst_pipeline_set_auto_flush_bus (GST_PIPELINE (pipeline), FALSE);
-  classification->bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  gst_bus_add_watch (classification->bus, gst_classification_handle_message,
-      classification);
-
-}
-
 static void
-process_inference (GstElement * element, gpointer inference_meta,
-    GstBuffer * buffer, gpointer user_data)
+gst_classification_process_inference (GstElement * element,
+    GstClassificationMeta * meta, GstBuffer * buffer, gpointer user_data)
 {
-
-  GstClassificationMeta *class_meta;
   gdouble current;
   gint index;
   gdouble max;
 
-  class_meta = (GstClassificationMeta *) inference_meta;
+  g_return_if_fail (element);
+  g_return_if_fail (meta);
+  g_return_if_fail (buffer);
+  g_return_if_fail (user_data);
+
   index = 0;
   max = -1;
 
-  for (gint i = 0; i < class_meta->num_labels; ++i) {
-    current = (class_meta->label_probs)[i];
+  for (gint i = 0; i < meta->num_labels; ++i) {
+    current = (meta->label_probs)[i];
     if (current > max) {
       max = current;
       index = i;
     }
   }
   g_print ("Highest probability is label %i : (%f) \n", index, max);
-
 }
 
 void
@@ -200,26 +196,34 @@ gst_classification_create_pipeline (GstClassification * classification)
   GString *pipe_desc;
   GstElement *pipeline;
   GError *error = NULL;
+  GstBus *bus;
+
+  g_return_if_fail (classification);
 
   pipe_desc = g_string_new ("");
 
-  g_string_append (pipe_desc, "inceptionv4 name=net ");
-  g_string_append (pipe_desc, "model-location=/home/user/graph.pb ");
-  g_string_append (pipe_desc, "backend::input-layer=input ");
-  g_string_append (pipe_desc,
-      "backend::output-layer=InceptionV4/Logits/Predictions ");
-  g_string_append (pipe_desc, "filesrc location=/home/user/cat.mp4 ! ");
-  g_string_append (pipe_desc, "decodebin ! videoconvert ! videoscale ! ");
-  g_string_append (pipe_desc, "video/x-raw, width=299, heigth=299 ! ");
-  g_string_append (pipe_desc, "tee name=t t. ! queue ! videoconvert ! ");
-  g_string_append (pipe_desc, "videoscale ! net.sink_model t. ! queue ! ");
-  g_string_append (pipe_desc, "net.sink_bypass net.src_bypass ! fakesink");
+  g_string_append (pipe_desc, " inceptionv4 name=net backend=");
+  g_string_append (pipe_desc, backend);
+  g_string_append (pipe_desc, " model-location=");
+  g_string_append (pipe_desc, model_path);
+  if (!strcmp (backend, "tensorflow")) {
+    g_string_append (pipe_desc, " backend::input-layer=input ");
+    g_string_append (pipe_desc,
+        " backend::output-layer=InceptionV4/Logits/Predictions ");
+  }
+  g_string_append (pipe_desc, " filesrc location=");
+  g_string_append (pipe_desc, file_path);
+  g_string_append (pipe_desc, " ! decodebin ! videoconvert ! videoscale ! ");
+  g_string_append (pipe_desc, " video/x-raw, width=299, heigth=299 ! ");
+  g_string_append (pipe_desc, " tee name=t t. ! queue ! videoconvert ! ");
+  g_string_append (pipe_desc, " videoscale ! net.sink_model t.! queue ! ");
+  g_string_append (pipe_desc, " net.sink_bypass net.src_bypass ! fakesink ");
 
   if (verbose)
     g_print ("pipeline: %s\n", pipe_desc->str);
 
-  pipeline = (GstElement *) gst_parse_launch (pipe_desc->str, &error);
-  g_string_free (pipe_desc, FALSE);
+  pipeline = GST_ELEMENT (gst_parse_launch (pipe_desc->str, &error));
+  g_string_free (pipe_desc, TRUE);
 
   if (error) {
     g_print ("pipeline parsing error: %s\n", error->message);
@@ -231,109 +235,80 @@ gst_classification_create_pipeline (GstClassification * classification)
   classification->pipeline = pipeline;
 
   gst_pipeline_set_auto_flush_bus (GST_PIPELINE (pipeline), FALSE);
-  classification->bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  gst_bus_add_watch (classification->bus, gst_classification_handle_message,
-      classification);
-
-  classification->inference_element =
-      gst_bin_get_by_name (GST_BIN (pipeline), "net");
-
-  g_signal_connect (classification->inference_element, "new-prediction",
-      G_CALLBACK (process_inference), NULL);
-
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+  gst_bus_add_watch (bus, gst_classification_handle_message, classification);
+  gst_object_unref (bus);
 }
 
 void
 gst_classification_start (GstClassification * classification)
 {
-  gst_element_set_state (classification->pipeline, GST_STATE_READY);
+  g_return_if_fail (classification);
 
-  classification->timer_id =
-      g_timeout_add (1000, onesecond_timer, classification);
+  gst_element_set_state (classification->pipeline, GST_STATE_PLAYING);
 }
 
 void
 gst_classification_stop (GstClassification * classification)
 {
-  gst_element_set_state (classification->pipeline, GST_STATE_NULL);
+  g_return_if_fail (classification);
 
-  g_source_remove (classification->timer_id);
+  gst_element_set_state (classification->pipeline, GST_STATE_NULL);
 }
 
 static void
 gst_classification_handle_eos (GstClassification * classification)
 {
-  gst_classification_stop (classification);
+  g_return_if_fail (classification);
+
+  g_main_loop_quit (classification->main_loop);
 }
 
 static void
 gst_classification_handle_error (GstClassification * classification,
     GError * error, const char *debug)
 {
-  g_print ("error: %s\n", error->message);
-  gst_classification_stop (classification);
+  g_return_if_fail (classification);
+  g_return_if_fail (error);
+  g_return_if_fail (debug);
+
+  g_printerr ("error: %s\n", error->message);
+  g_main_loop_quit (classification->main_loop);
 }
 
 static void
 gst_classification_handle_warning (GstClassification * classification,
     GError * error, const char *debug)
 {
-  g_print ("warning: %s\n", error->message);
+  g_return_if_fail (classification);
+  g_return_if_fail (error);
+  g_return_if_fail (debug);
+
+  g_printerr ("warning: %s\n", error->message);
 }
 
 static void
 gst_classification_handle_info (GstClassification * classification,
     GError * error, const char *debug)
 {
+  g_return_if_fail (classification);
+  g_return_if_fail (error);
+  g_return_if_fail (debug);
+
   g_print ("info: %s\n", error->message);
 }
-
-static void
-gst_classification_handle_null_to_ready (GstClassification * classification)
-{
-  gst_element_set_state (classification->pipeline, GST_STATE_PAUSED);
-
-}
-
-static void
-gst_classification_handle_ready_to_paused (GstClassification * classification)
-{
-  if (!classification->paused_for_buffering) {
-    gst_element_set_state (classification->pipeline, GST_STATE_PLAYING);
-  }
-}
-
-static void
-gst_classification_handle_paused_to_playing (GstClassification * classification)
-{
-
-}
-
-static void
-gst_classification_handle_playing_to_paused (GstClassification * classification)
-{
-
-}
-
-static void
-gst_classification_handle_paused_to_ready (GstClassification * classification)
-{
-
-}
-
-static void
-gst_classification_handle_ready_to_null (GstClassification * classification)
-{
-  g_main_loop_quit (classification->main_loop);
-
-}
-
 
 static gboolean
 gst_classification_handle_message (GstBus * bus, GstMessage * message,
     gpointer data)
 {
-  GstClassification *classification = (GstClassification *) data;
+  GstClassification *classification;
+
+  g_return_val_if_fail (bus, FALSE);
+  g_return_val_if_fail (message, FALSE);
+  g_return_val_if_fail (data, FALSE);
+
+  classification = (GstClassification *) data;
 
   switch (GST_MESSAGE_TYPE (message)) {
     case GST_MESSAGE_EOS:
@@ -369,87 +344,6 @@ gst_classification_handle_message (GstBus * bus, GstMessage * message,
       g_clear_error (&error);
     }
       break;
-    case GST_MESSAGE_TAG:
-    {
-      GstTagList *tag_list;
-
-      gst_message_parse_tag (message, &tag_list);
-      if (verbose)
-        g_print ("tag\n");
-    }
-      break;
-    case GST_MESSAGE_STATE_CHANGED:
-    {
-      GstState oldstate, newstate, pending;
-
-      gst_message_parse_state_changed (message, &oldstate, &newstate, &pending);
-      if (GST_ELEMENT (message->src) == classification->pipeline) {
-        if (verbose)
-          g_print ("state change from %s to %s\n",
-              gst_element_state_get_name (oldstate),
-              gst_element_state_get_name (newstate));
-        switch (GST_STATE_TRANSITION (oldstate, newstate)) {
-          case GST_STATE_CHANGE_NULL_TO_READY:
-            gst_classification_handle_null_to_ready (classification);
-            break;
-          case GST_STATE_CHANGE_READY_TO_PAUSED:
-            gst_classification_handle_ready_to_paused (classification);
-            break;
-          case GST_STATE_CHANGE_PAUSED_TO_PLAYING:
-            gst_classification_handle_paused_to_playing (classification);
-            break;
-          case GST_STATE_CHANGE_PLAYING_TO_PAUSED:
-            gst_classification_handle_playing_to_paused (classification);
-            break;
-          case GST_STATE_CHANGE_PAUSED_TO_READY:
-            gst_classification_handle_paused_to_ready (classification);
-            break;
-          case GST_STATE_CHANGE_READY_TO_NULL:
-            gst_classification_handle_ready_to_null (classification);
-            break;
-          default:
-            if (verbose)
-              g_print ("unknown state change from %s to %s\n",
-                  gst_element_state_get_name (oldstate),
-                  gst_element_state_get_name (newstate));
-        }
-      }
-    }
-      break;
-    case GST_MESSAGE_BUFFERING:
-    {
-      int percent;
-      gst_message_parse_buffering (message, &percent);
-      //g_print("buffering %d\n", percent);
-      if (!classification->paused_for_buffering && percent < 100) {
-        g_print ("pausing for buffing\n");
-        classification->paused_for_buffering = TRUE;
-        gst_element_set_state (classification->pipeline, GST_STATE_PAUSED);
-      } else if (classification->paused_for_buffering && percent == 100) {
-        g_print ("unpausing for buffing\n");
-        classification->paused_for_buffering = FALSE;
-        gst_element_set_state (classification->pipeline, GST_STATE_PLAYING);
-      }
-    }
-      break;
-    case GST_MESSAGE_STATE_DIRTY:
-    case GST_MESSAGE_CLOCK_PROVIDE:
-    case GST_MESSAGE_CLOCK_LOST:
-    case GST_MESSAGE_NEW_CLOCK:
-    case GST_MESSAGE_STRUCTURE_CHANGE:
-    case GST_MESSAGE_STREAM_STATUS:
-      break;
-    case GST_MESSAGE_STEP_DONE:
-    case GST_MESSAGE_APPLICATION:
-    case GST_MESSAGE_ELEMENT:
-    case GST_MESSAGE_SEGMENT_START:
-    case GST_MESSAGE_SEGMENT_DONE:
-    case GST_MESSAGE_LATENCY:
-    case GST_MESSAGE_ASYNC_START:
-    case GST_MESSAGE_ASYNC_DONE:
-    case GST_MESSAGE_REQUEST_STATE:
-    case GST_MESSAGE_STEP_START:
-    case GST_MESSAGE_QOS:
     default:
       if (verbose) {
         g_print ("message: %s\n", GST_MESSAGE_TYPE_NAME (message));
@@ -459,32 +353,3 @@ gst_classification_handle_message (GstBus * bus, GstMessage * message,
 
   return TRUE;
 }
-
-
-
-static gboolean
-onesecond_timer (gpointer priv)
-{
-
-  return TRUE;
-}
-
-
-
-/* helper functions */
-
-#if 0
-gboolean
-have_element (const gchar * element_name)
-{
-  GstPluginFeature *feature;
-
-  feature = gst_default_registry_find_feature (element_name,
-      GST_TYPE_ELEMENT_FACTORY);
-  if (feature) {
-    g_object_unref (feature);
-    return TRUE;
-  }
-  return FALSE;
-}
-#endif
