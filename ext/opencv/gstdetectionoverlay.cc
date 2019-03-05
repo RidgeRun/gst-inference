47316a9e7169edc4fa25346ca80c53cad057cb71
Miguel Taylor <miguel.taylor@ridgerun.com>
2019-03-05 11:19:04 -0600

    Add detection box properties

--- ext/opencv/gstdetectionoverlay.cc
+++ ext/opencv/gstdetectionoverlay.cc
@@ -34,6 +34,13 @@
 GST_DEBUG_CATEGORY_STATIC (gst_detection_overlay_debug_category);
 #define GST_CAT_DEFAULT gst_detection_overlay_debug_category
 
+#define MIN_FONT_SCALE 0
+#define DEFAULT_FONT_SCALE 2
+#define MAX_FONT_SCALE 100
+#define MIN_BOX_THICKNESS 1
+#define DEFAULT_BOX_THICKNESS 2
+#define MAX_BOX_THICKNESS 100
+
 #define N_C 20
 const cv::Scalar colors[] = {
   cv::Scalar (254, 254, 254), cv::Scalar (239, 211, 127),
@@ -70,7 +77,9 @@ gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
     GstVideoFrame *frame);
 
 enum {
-  PROP_0
+  PROP_0,
+  PROP_FONT_SCALE,
+  PROP_BOX_THICKNESS
 };
 
 /* pad templates */
@@ -118,6 +127,16 @@ gst_detection_overlay_class_init (GstDetectionOverlayClass *klass) {
   gobject_class->get_property = gst_detection_overlay_get_property;
   gobject_class->dispose = gst_detection_overlay_dispose;
   gobject_class->finalize = gst_detection_overlay_finalize;
+
+  g_object_class_install_property (gobject_class, PROP_FONT_SCALE,
+                                   g_param_spec_double ("font-scale", "font", "Font scale", MIN_FONT_SCALE,
+                                       MAX_FONT_SCALE, DEFAULT_FONT_SCALE, G_PARAM_READWRITE));
+
+  g_object_class_install_property (gobject_class, PROP_BOX_THICKNESS,
+                                   g_param_spec_int ("thickness", "thickness", "Box line thickness in pixels",
+                                       MIN_BOX_THICKNESS,
+                                       MAX_BOX_THICKNESS, DEFAULT_BOX_THICKNESS, G_PARAM_READWRITE));
+
   base_transform_class->start = GST_DEBUG_FUNCPTR (gst_detection_overlay_start);
   base_transform_class->stop = GST_DEBUG_FUNCPTR (gst_detection_overlay_stop);
   video_filter_class->transform_frame_ip =
@@ -127,6 +146,8 @@ gst_detection_overlay_class_init (GstDetectionOverlayClass *klass) {
 
 static void
 gst_detection_overlay_init (GstDetectionOverlay *detection_overlay) {
+  detection_overlay->font_scale = DEFAULT_FONT_SCALE;
+  detection_overlay->box_thickness = DEFAULT_BOX_THICKNESS;
 }
 
 void
@@ -137,6 +158,16 @@ gst_detection_overlay_set_property (GObject *object, guint property_id,
   GST_DEBUG_OBJECT (detection_overlay, "set_property");
 
   switch (property_id) {
+    case PROP_FONT_SCALE:
+      detection_overlay->font_scale = g_value_get_double (value);
+      GST_DEBUG_OBJECT (detection_overlay, "Changed font scale to %lf",
+                        detection_overlay->font_scale);
+      break;
+    case PROP_BOX_THICKNESS:
+      detection_overlay->box_thickness = g_value_get_int (value);
+      GST_DEBUG_OBJECT (detection_overlay, "Changed box thickness to %d",
+                        detection_overlay->box_thickness);
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
       break;
@@ -151,6 +182,12 @@ gst_detection_overlay_get_property (GObject *object, guint property_id,
   GST_DEBUG_OBJECT (detection_overlay, "get_property");
 
   switch (property_id) {
+    case PROP_FONT_SCALE:
+      g_value_set_double (value, detection_overlay->font_scale);
+      break;
+    case PROP_BOX_THICKNESS:
+      g_value_set_int (value, detection_overlay->box_thickness);
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
       break;
@@ -201,15 +238,13 @@ gst_detection_overlay_stop (GstBaseTransform *trans) {
 static GstFlowReturn
 gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
     GstVideoFrame *frame) {
+  GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (trans);
   GstDetectionMeta *detect_meta;
   gint i, width, height;
   cv::Mat cv_mat;
   cv::Size size;
   BBox box;
   const gint bpp = 3;
-
-  gdouble font_scale = 1;
-  gint thickness = 1;
 
   width = GST_VIDEO_FRAME_COMP_STRIDE (frame, 0) / bpp;
   height = GST_VIDEO_FRAME_HEIGHT (frame);
@@ -230,10 +265,11 @@ gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
     /* Put string on screen */
     cv::putText (cv_mat, classes[box.label % N_C], cv::Point (box.x, box.y - 5),
                  cv::FONT_HERSHEY_PLAIN,
-                 font_scale, colors[box.label % N_C], thickness);
+                 detection_overlay->font_scale, colors[box.label % N_C],
+                 detection_overlay->box_thickness);
     cv::rectangle (cv_mat, cv::Point (box.x, box.y),
                    cv::Point (box.x + box.width, box.y + box.height), colors[box.label % N_C],
-                   thickness);
+                   detection_overlay->box_thickness);
   }
 
   return GST_FLOW_OK;
