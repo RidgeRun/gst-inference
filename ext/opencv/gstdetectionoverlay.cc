2797079d7c7d03b2262dd5b2dc7f373563f14296
Miguel Taylor <miguel.taylor@ridgerun.com>
2019-03-07 18:13:50 -0600

    Fix indentation

--- ext/opencv/gstdetectionoverlay.cc
+++ ext/opencv/gstdetectionoverlay.cc
@@ -25,10 +25,10 @@
 #include "gstdetectionoverlay.h"
 #include "gst/r2inference/gstinferencemeta.h"
 #ifdef OCV_VERSION_LT_3_2
-  #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #else
-  #include "opencv2/imgproc.hpp"
-  #include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 #endif
 
 GST_DEBUG_CATEGORY_STATIC (gst_detection_overlay_debug_category);
@@ -60,20 +60,21 @@ const cv::Scalar colors[] = {
 /* prototypes */
 
 
-static void gst_detection_overlay_set_property (GObject *object,
-    guint property_id, const GValue *value, GParamSpec *pspec);
-static void gst_detection_overlay_get_property (GObject *object,
-    guint property_id, GValue *value, GParamSpec *pspec);
-static void gst_detection_overlay_dispose (GObject *object);
-static void gst_detection_overlay_finalize (GObject *object);
+static void gst_detection_overlay_set_property (GObject * object,
+    guint property_id, const GValue * value, GParamSpec * pspec);
+static void gst_detection_overlay_get_property (GObject * object,
+    guint property_id, GValue * value, GParamSpec * pspec);
+static void gst_detection_overlay_dispose (GObject * object);
+static void gst_detection_overlay_finalize (GObject * object);
 
-static gboolean gst_detection_overlay_start (GstBaseTransform *trans);
-static gboolean gst_detection_overlay_stop (GstBaseTransform *trans);
+static gboolean gst_detection_overlay_start (GstBaseTransform * trans);
+static gboolean gst_detection_overlay_stop (GstBaseTransform * trans);
 static GstFlowReturn
-gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
-    GstVideoFrame *frame);
+gst_detection_overlay_transform_frame_ip (GstVideoFilter * trans,
+    GstVideoFrame * frame);
 
-enum {
+enum
+{
   PROP_0,
   PROP_FONT_SCALE,
   PROP_BOX_THICKNESS,
@@ -92,33 +93,35 @@ enum {
 /* class initialization */
 
 G_DEFINE_TYPE_WITH_CODE (GstDetectionOverlay, gst_detection_overlay,
-                         GST_TYPE_VIDEO_FILTER,
-                         GST_DEBUG_CATEGORY_INIT (gst_detection_overlay_debug_category,
-                             "detection_overlay", 0,
-                             "debug category for detection_overlay element"));
+    GST_TYPE_VIDEO_FILTER,
+    GST_DEBUG_CATEGORY_INIT (gst_detection_overlay_debug_category,
+        "detection_overlay", 0,
+        "debug category for detection_overlay element"));
 
 static void
-gst_detection_overlay_class_init (GstDetectionOverlayClass *klass) {
+gst_detection_overlay_class_init (GstDetectionOverlayClass * klass)
+{
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-  GstBaseTransformClass *base_transform_class = GST_BASE_TRANSFORM_CLASS (klass);
+  GstBaseTransformClass *base_transform_class =
+      GST_BASE_TRANSFORM_CLASS (klass);
   GstVideoFilterClass *video_filter_class = GST_VIDEO_FILTER_CLASS (klass);
 
   gst_element_class_add_pad_template (GST_ELEMENT_CLASS (klass),
-                                      gst_pad_template_new ("src", GST_PAD_SRC, GST_PAD_ALWAYS,
-                                          gst_caps_from_string (VIDEO_SRC_CAPS)));
+      gst_pad_template_new ("src", GST_PAD_SRC, GST_PAD_ALWAYS,
+          gst_caps_from_string (VIDEO_SRC_CAPS)));
   gst_element_class_add_pad_template (GST_ELEMENT_CLASS (klass),
-                                      gst_pad_template_new ("sink", GST_PAD_SINK, GST_PAD_ALWAYS,
-                                          gst_caps_from_string (VIDEO_SINK_CAPS)));
+      gst_pad_template_new ("sink", GST_PAD_SINK, GST_PAD_ALWAYS,
+          gst_caps_from_string (VIDEO_SINK_CAPS)));
 
   gst_element_class_set_static_metadata (GST_ELEMENT_CLASS (klass),
-                                         "detectionoverlay", "Filter",
-                                         "Overlays detection metadata on input buffer",
-                                         "Carlos Rodriguez <carlos.rodriguez@ridgerun.com> \n\t\t\t"
-                                         "   Jose Jimenez <jose.jimenez@ridgerun.com> \n\t\t\t"
-                                         "   Michael Gruner <michael.gruner@ridgerun.com> \n\t\t\t"
-                                         "   Carlos Aguero <carlos.aguero@ridgerun.com> \n\t\t\t"
-                                         "   Miguel Taylor <miguel.taylor@ridgerun.com> \n\t\t\t"
-                                         "   Greivin Fallas <greivin.fallas@ridgerun.com>");
+      "detectionoverlay", "Filter",
+      "Overlays detection metadata on input buffer",
+      "Carlos Rodriguez <carlos.rodriguez@ridgerun.com> \n\t\t\t"
+      "   Jose Jimenez <jose.jimenez@ridgerun.com> \n\t\t\t"
+      "   Michael Gruner <michael.gruner@ridgerun.com> \n\t\t\t"
+      "   Carlos Aguero <carlos.aguero@ridgerun.com> \n\t\t\t"
+      "   Miguel Taylor <miguel.taylor@ridgerun.com> \n\t\t\t"
+      "   Greivin Fallas <greivin.fallas@ridgerun.com>");
 
   gobject_class->set_property = gst_detection_overlay_set_property;
   gobject_class->get_property = gst_detection_overlay_get_property;
@@ -126,28 +129,29 @@ gst_detection_overlay_class_init (GstDetectionOverlayClass *klass) {
   gobject_class->finalize = gst_detection_overlay_finalize;
 
   g_object_class_install_property (gobject_class, PROP_FONT_SCALE,
-                                   g_param_spec_double ("font-scale", "font", "Font scale", MIN_FONT_SCALE,
-                                       MAX_FONT_SCALE, DEFAULT_FONT_SCALE, G_PARAM_READWRITE));
+      g_param_spec_double ("font-scale", "font", "Font scale", MIN_FONT_SCALE,
+          MAX_FONT_SCALE, DEFAULT_FONT_SCALE, G_PARAM_READWRITE));
 
   g_object_class_install_property (gobject_class, PROP_BOX_THICKNESS,
-                                   g_param_spec_int ("thickness", "thickness", "Box line thickness in pixels",
-                                       MIN_BOX_THICKNESS, MAX_BOX_THICKNESS, DEFAULT_BOX_THICKNESS,
-                                       G_PARAM_READWRITE));
+      g_param_spec_int ("thickness", "thickness",
+          "Box line thickness in pixels", MIN_BOX_THICKNESS, MAX_BOX_THICKNESS,
+          DEFAULT_BOX_THICKNESS, G_PARAM_READWRITE));
 
   g_object_class_install_property (gobject_class, PROP_LABELS,
-                                   g_param_spec_string ("labels", "labels",
-                                       "Semicolon separated string containing inference labels", DEFAULT_LABELS,
-                                       G_PARAM_READWRITE));
+      g_param_spec_string ("labels", "labels",
+          "Semicolon separated string containing inference labels",
+          DEFAULT_LABELS, G_PARAM_READWRITE));
 
   base_transform_class->start = GST_DEBUG_FUNCPTR (gst_detection_overlay_start);
   base_transform_class->stop = GST_DEBUG_FUNCPTR (gst_detection_overlay_stop);
-  video_filter_class->transform_frame_ip = GST_DEBUG_FUNCPTR (
-        gst_detection_overlay_transform_frame_ip);
+  video_filter_class->transform_frame_ip =
+      GST_DEBUG_FUNCPTR (gst_detection_overlay_transform_frame_ip);
 
 }
 
 static void
-gst_detection_overlay_init (GstDetectionOverlay *detection_overlay) {
+gst_detection_overlay_init (GstDetectionOverlay * detection_overlay)
+{
   detection_overlay->font_scale = DEFAULT_FONT_SCALE;
   detection_overlay->box_thickness = DEFAULT_BOX_THICKNESS;
   detection_overlay->labels = DEFAULT_LABELS;
@@ -156,8 +160,9 @@ gst_detection_overlay_init (GstDetectionOverlay *detection_overlay) {
 }
 
 void
-gst_detection_overlay_set_property (GObject *object, guint property_id,
-                                    const GValue *value, GParamSpec *pspec) {
+gst_detection_overlay_set_property (GObject * object, guint property_id,
+    const GValue * value, GParamSpec * pspec)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (object);
 
   GST_DEBUG_OBJECT (detection_overlay, "set_property");
@@ -166,12 +171,12 @@ gst_detection_overlay_set_property (GObject *object, guint property_id,
     case PROP_FONT_SCALE:
       detection_overlay->font_scale = g_value_get_double (value);
       GST_DEBUG_OBJECT (detection_overlay, "Changed font scale to %lf",
-                        detection_overlay->font_scale);
+          detection_overlay->font_scale);
       break;
     case PROP_BOX_THICKNESS:
       detection_overlay->box_thickness = g_value_get_int (value);
       GST_DEBUG_OBJECT (detection_overlay, "Changed box thickness to %d",
-                        detection_overlay->box_thickness);
+          detection_overlay->box_thickness);
       break;
     case PROP_LABELS:
       if (detection_overlay->labels != NULL) {
@@ -181,10 +186,12 @@ gst_detection_overlay_set_property (GObject *object, guint property_id,
         g_strfreev (detection_overlay->labels_list);
       }
       detection_overlay->labels = g_value_dup_string (value);
-      detection_overlay->labels_list = g_strsplit (g_value_get_string (value), ";", 0);
-      detection_overlay->num_labels = g_strv_length (detection_overlay->labels_list);
+      detection_overlay->labels_list =
+          g_strsplit (g_value_get_string (value), ";", 0);
+      detection_overlay->num_labels =
+          g_strv_length (detection_overlay->labels_list);
       GST_DEBUG_OBJECT (detection_overlay, "Changed inference labels %s",
-                        detection_overlay->labels);
+          detection_overlay->labels);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -193,8 +200,9 @@ gst_detection_overlay_set_property (GObject *object, guint property_id,
 }
 
 void
-gst_detection_overlay_get_property (GObject *object, guint property_id,
-                                    GValue *value, GParamSpec *pspec) {
+gst_detection_overlay_get_property (GObject * object, guint property_id,
+    GValue * value, GParamSpec * pspec)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (object);
 
   GST_DEBUG_OBJECT (detection_overlay, "get_property");
@@ -216,7 +224,8 @@ gst_detection_overlay_get_property (GObject *object, guint property_id,
 }
 
 void
-gst_detection_overlay_dispose (GObject *object) {
+gst_detection_overlay_dispose (GObject * object)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (object);
 
   GST_DEBUG_OBJECT (detection_overlay, "dispose");
@@ -233,7 +242,8 @@ gst_detection_overlay_dispose (GObject *object) {
 }
 
 void
-gst_detection_overlay_finalize (GObject *object) {
+gst_detection_overlay_finalize (GObject * object)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (object);
 
   GST_DEBUG_OBJECT (detection_overlay, "finalize");
@@ -244,7 +254,8 @@ gst_detection_overlay_finalize (GObject *object) {
 }
 
 static gboolean
-gst_detection_overlay_start (GstBaseTransform *trans) {
+gst_detection_overlay_start (GstBaseTransform * trans)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (trans);
 
   GST_DEBUG_OBJECT (detection_overlay, "start");
@@ -253,7 +264,8 @@ gst_detection_overlay_start (GstBaseTransform *trans) {
 }
 
 static gboolean
-gst_detection_overlay_stop (GstBaseTransform *trans) {
+gst_detection_overlay_stop (GstBaseTransform * trans)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (trans);
 
   GST_DEBUG_OBJECT (detection_overlay, "stop");
@@ -263,8 +275,9 @@ gst_detection_overlay_stop (GstBaseTransform *trans) {
 
 /* transform */
 static GstFlowReturn
-gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
-    GstVideoFrame *frame) {
+gst_detection_overlay_transform_frame_ip (GstVideoFilter * trans,
+    GstVideoFrame * frame)
+{
   GstDetectionOverlay *detection_overlay = GST_DETECTION_OVERLAY (trans);
   GstDetectionMeta *detect_meta;
   gint i, width, height;
@@ -278,8 +291,8 @@ gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
   height = GST_VIDEO_FRAME_HEIGHT (frame);
 
   detect_meta =
-    (GstDetectionMeta *) gst_buffer_get_meta (frame->buffer,
-        GST_DETECTION_META_API_TYPE);
+      (GstDetectionMeta *) gst_buffer_get_meta (frame->buffer,
+      GST_DETECTION_META_API_TYPE);
   if (NULL == detect_meta) {
     GST_LOG_OBJECT (trans, "No detection meta found");
     return GST_FLOW_OK;
@@ -298,11 +311,12 @@ gst_detection_overlay_transform_frame_ip (GstVideoFilter *trans,
     }
 
     /* Put string on screen */
-    cv::putText (cv_mat, str, cv::Point (box.x, box.y - 5), cv::FONT_HERSHEY_PLAIN,
-                 detection_overlay->font_scale, colors[box.label % N_C],
-                 detection_overlay->box_thickness);
-    cv::rectangle (cv_mat, cv::Point (box.x, box.y), cv::Point (box.x + box.width,
-                   box.y + box.height), colors[box.label % N_C], detection_overlay->box_thickness);
+    cv::putText (cv_mat, str, cv::Point (box.x, box.y - 5),
+        cv::FONT_HERSHEY_PLAIN, detection_overlay->font_scale,
+        colors[box.label % N_C], detection_overlay->box_thickness);
+    cv::rectangle (cv_mat, cv::Point (box.x, box.y),
+        cv::Point (box.x + box.width, box.y + box.height),
+        colors[box.label % N_C], detection_overlay->box_thickness);
   }
 
   return GST_FLOW_OK;
