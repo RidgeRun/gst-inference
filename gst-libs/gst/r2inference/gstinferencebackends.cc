905fe009bde38db4516f054f2ceba6cf38cc07d9
Melissa Montero <melissa.montero@ridgerun.com>
2020-06-18 20:04:20 -0600

    gstinferencebackends: use r2i framework's name to get the backend object

--- gst-libs/gst/r2inference/gstinferencebackends.cc
+++ gst-libs/gst/r2inference/gstinferencebackends.cc
@@ -30,18 +30,6 @@
 
 #define DEFAULT_ALIGNMENT 32
 
-static std::unordered_map <int,GType>
-backend_types ({
-  {r2i::FrameworkCode::EDGETPU, GST_TYPE_EDGETPU},
-  {r2i::FrameworkCode::NCSDK, GST_TYPE_NCSDK},
-  {r2i::FrameworkCode::TENSORFLOW, GST_TYPE_TENSORFLOW},
-  {r2i::FrameworkCode::TFLITE, GST_TYPE_TFLITE},
-  {r2i::FrameworkCode::TENSORRT, GST_TYPE_TENSORRT},
-  {r2i::FrameworkCode::MAX_FRAMEWORK, G_TYPE_INVALID}
-});
-
-const gchar *gst_inference_backends_search_type_name (guint id);
-
 static void
 gst_inference_backends_add_frameworkmeta (r2i::FrameworkMeta meta,
     gchar ** backends_parameters, r2i::RuntimeError error, guint alignment);
@@ -49,6 +37,8 @@ gst_inference_backends_add_frameworkmeta (r2i::FrameworkMeta meta,
 static void
 gst_inference_backends_enum_register_item (const guint id,
     const gchar * desc, const gchar * shortname);
+
+static gchar *gst_inference_backends_get_type_name (const gchar * framework);
 
 static GEnumValue *backend_enum_desc = NULL;
 
@@ -89,30 +79,33 @@ gst_inference_backends_enum_register_item (const guint id,
   backend_desc->value_nick = NULL;
 }
 
-const gchar *
-gst_inference_backends_search_type_name (guint id)
+static gchar *
+gst_inference_backends_get_type_name (const gchar * framework)
 {
-  auto search = backend_types.find (id);
-  if (backend_types.end () == search) {
-    search = backend_types.find (r2i::FrameworkCode::MAX_FRAMEWORK);
-  }
-  return search->second;
+  return g_strdup_printf ("Gst%s", framework);
 }
 
 
 GType
 gst_inference_backends_search_type (guint id)
 {
-  const gchar *backend_type_name;
-  GType backend_type;
+  const gchar *framework = NULL;
+  gchar *backend_type_name = NULL;
+  GType backend_type = G_TYPE_INVALID;
+  GEnumClass *enum_class = NULL;
+  GEnumValue *enum_value = NULL;
 
-  backend_type_name = gst_inference_backends_search_type_name (id);
+  enum_class = (GEnumClass *) g_type_class_ref (GST_TYPE_INFERENCE_BACKENDS);
+  enum_value = g_enum_get_value (enum_class, id);
 
-  if (!backend_type_name) {
-    backend_type = G_TYPE_INVALID;
-  } else {
+  if (enum_value) {
+    framework = enum_value->value_nick;
+    backend_type_name = gst_inference_backends_get_type_name (framework);
     backend_type = g_type_from_name (backend_type_name);
+    g_free (backend_type_name);
   }
+
+  g_type_class_unref (enum_class);
 
   return backend_type;
 }
@@ -123,13 +116,13 @@ gst_inference_backends_add_frameworkmeta (r2i::FrameworkMeta meta,
 {
   GstBackend *backend = NULL;
   gchar *parameters, *backend_name;
-  const gchar *backend_type_name;
+  gchar *backend_type_name;
   GType backend_type;
 
   gst_inference_backends_enum_register_item (meta.code,
       meta.description.c_str (), meta.name.c_str ());
 
-  backend_type_name = g_strdup_printf ("Gst%s", meta.name.c_str ());
+  backend_type_name = gst_inference_backends_get_type_name (meta.name.c_str ());
   if (NULL == backend_type_name) {
     GST_ERROR_OBJECT (backend, "Failed to find Backend type: %s",
         meta.name.c_str ());
@@ -138,6 +131,7 @@ gst_inference_backends_add_frameworkmeta (r2i::FrameworkMeta meta,
   gst_inference_backend_register (backend_type_name, meta.code);
   backend_type = g_type_from_name (backend_type_name);
   backend = (GstBackend *) g_object_new (backend_type, NULL);
+  g_free (backend_type_name);
 
   backend_name =
       g_strdup_printf ("%*s: %s. Version: %s\n", alignment, meta.name.c_str (),
